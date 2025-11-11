a8be82f6ec9223a1f2e9f1378b062ed599646635
Xavi <xavicervera03@gmail.com>
2025-11-11 14:10:39 +0100

    Samurais completada

---
+++ src/main/java/org/example/BateriaRandom/Ej1.java
@@ -0,0 +1,18 @@
+package org.example.BateriaRandom;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ej1 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        Random random = new Random();
+
+        
+
+
+    }
+
+}
